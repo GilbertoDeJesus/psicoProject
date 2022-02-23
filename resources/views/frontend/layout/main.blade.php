024e3ce750baf908c3efde87549c83a110106bf7
GilbertoDeJesus <gadjs2001@gmail.com>
2022-02-23 03:38:49 -0600

    Creacion de menu superior de cuestionarios

---
+++ resources/views/frontend/layout/main.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ url('frontend/assets/img/apple-icon.png') }}">
+    <link rel="icon" type="image/png" href="{{ url('frontend/assets/img/favicon.png') }}">
+    <title>
+        Registro de alumnos
+    </title>
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <!-- Nucleo Icons -->
+    <link href="{{ url('frontend/assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+    <link href="{{ url('frontend/assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link href="{{ url('frontend/assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+    <!-- CSS Files -->
+    <link href="{{ url('frontend/testsAssets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ url('frontend/testsAssets/css/menu.css') }}" rel="stylesheet">
+    <link href="{{ url('frontend/testsAssets/css/style.css') }}" rel="stylesheet">
+    <link href="{{ url('frontend/testsAssets/css/vendors.css') }}" rel="stylesheet">
+
+    <!-- YOUR CUSTOM CSS -->
+    <link href="{{ url('frontend/testsAssets/css/custom.css') }}" rel="stylesheet">
+    <link href="{{ url('frontend/assets/css/soft-ui-dashboard.css?v=1.0.3') }}" rel="stylesheet" />
+    <!-- MODERNIZR MENU -->
+    <script src="{{ url('frontend/testsAssets/js/modernizr.js') }}"></script>
+
+</head>
+
+<body class="">
+    <div class="container position-sticky z-index-sticky top-0">
+        <!-- Navbar -->
+        @include('frontend.layout.topMenu')
+        <!-- End Navbar -->
+    </div>
+    <main class="main-content  mt-0">
+        <section>
+            <div class="page-header min-vh-100">
+                @yield('contenido')
+            </div>
+        </section>
+    </main>
+    <!--   Core JS Files   -->
+    <script src="{{ url('frontend/assets/js/core/popper.min.js') }}"></script>
+    <script src="{{ url('frontend/assets/js/core/bootstrap.min.js') }}"></script>
+    <script src="{{ url('frontend/testsAssets/js/jquery-3.2.1.min.j') }}s"></script>
+    <script src="{{ url('frontend/testsAssets/js/common_scripts.min.js') }}"></script>
+    <script src="{{ url('frontend/testsAssets/js/velocity.min.js') }}"></script>
+    <script src="{{ url('frontend/testsAssets/js/functions.js') }}"></script>
+
+    <!-- Wizard script -->
+    <script src="{{ url('frontend/testsAssets/js/survey_func.js') }}"></script>
+    <script>
+        var win = navigator.platform.indexOf('Win') > -1;
+        if (win && document.querySelector('#sidenav-scrollbar')) {
+            var options = {
+                damping: '0.5'
+            }
+            Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+        }
+    </script>
+    <!-- Github buttons -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+</body>
+
+</html>
---
+++ resources/views/frontend/layout/topMenu.blade.php
@@ -0,0 +1,72 @@
+<nav
+    class="navbar navbar-expand-lg blur blur-rounded top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
+    <div class="container-fluid">
+        <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="#">
+            Soft UI Dashboard
+        </a>
+        <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon mt-2">
+                <span class="navbar-toggler-bar bar1"></span>
+                <span class="navbar-toggler-bar bar2"></span>
+                <span class="navbar-toggler-bar bar3"></span>
+            </span>
+        </button>
+        <div class="collapse navbar-collapse" id="navigation">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <a class="nav-link d-flex align-items-center me-2 active" aria-current="page"
+                        href="#">
+                        <i class="fa fa-chart-pie opacity-6 text-dark me-1"></i>
+                        C. Vocacional
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link me-2" href="#">
+                        <i class="fa fa-user opacity-6 text-dark me-1"></i>
+                        C. Trayectoria
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link me-2" href="#">
+                        <i class="fas fa-user-circle opacity-6 text-dark me-1"></i>
+                        C. Aprendizaje
+                    </a>
+                </li>
+                <li class="nav-item d-lg-none d-block">
+                    <a class="nav-link me-2" href="#">
+                        <i class="fas fa-key opacity-6 text-dark me-1"></i>
+                        Cerrar sesión
+                    </a>
+                </li>
+            </ul>
+            <ul class="navbar-nav d-lg-block d-none">
+                <li class="nav-item dropdown pe-2 d-flex align-items-center">
+                    <a href="javascript:;" class="btn btn-icon btn-3 btn-round mb-0 me-1 bg-gradient-dark" id="dropdownMenuButton"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        <span class="btn-inner--icon"><i class="fas fa-user-circle me-1"></i></span>
+                        Jhon Doe
+                    </a>
+                    <ul class="dropdown-menu  dropdown-menu-end  px-2 py-3 me-sm-n4"
+                        aria-labelledby="dropdownMenuButton">
+                        <li>
+                            <a class="dropdown-item border-radius-md" href="#">
+                                <div class="d-flex py-1">
+                                    <div class="avatar avatar-sm bg-gradient-dark  me-3  my-auto">
+                                        <i class="fa fa-user opacity-6 text-white"></i>
+                                    </div>
+                                    <div class="d-flex flex-column justify-content-center">
+                                        <h6 class="text-sm font-weight-normal mb-0">
+                                            Cerrar sesión
+                                        </h6>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
