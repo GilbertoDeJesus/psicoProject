afbdd6aab122e40e889cb8604a31372d49adaedb
angeltorres23 <angeltorresdiaz23@gmail.com>
2022-03-14 14:23:01 -0600

    Preguntas filtradas por test

--- app/Http/Controllers/frontend/LearningTestController.php
+++ app/Http/Controllers/frontend/LearningTestController.php
@@ -3,16 +3,22 @@
 namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Question;
+use App\Models\Test;
 use Illuminate\Http\Request;
 
 class LearningTestController extends Controller
 {
     public function index(){
 
-        $learningStyle= \DB:: table('questions')
-        ->select('questions.*')
-        ->orderBy('order','ASC')
-        ->get();
+        $test_id = Test::query()
+            ->where('name', 'LIKE', "%aprendizaje%")
+            ->orderByDesc('id')
+            ->first();
+
+        $test = Test::where('id',$test_id->id)->orderBy('id', 'ASC')->first();
+        $learningStyle = $test->questions()->orderBy('order', 'ASC')->get();
+        
          return view('frontend.learningStyle.learningTest')->with('learningTest', $learningStyle);
 
     }
